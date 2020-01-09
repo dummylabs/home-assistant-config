2de6af51b1d65d8387879ed826da2d76f8b11104
dummylabs <scratcha@gmail.com>
2020-01-09 10:46:38 +0000

    humidifier updates

--- appdaemon/apps/humidifier/humidifier.py
+++ appdaemon/apps/humidifier/humidifier.py
@@ -4,12 +4,12 @@ import datetime
 class Humidifier(hass.Hass):
     def initialize(self):
         self.run_once(self.switch_on, datetime.time(10, 0, 0))
-        self.run_once(self.switch_off, datetime.time(21, 30, 0))
+        self.run_once(self.switch_off, datetime.time(23, 00, 0))
 
     def switch_on(self, kwargs):
-        self.call_service('fan/turn_on', entity_id="fan.xiaomi_humidifier")
+        self.call_service('fan/turn_on', entity_id="fan.zhimi_humidifier_ca1")
         self.log('Humidifier switched on')
 
     def switch_off(self, kwargs):
-        self.call_service('fan/turn_off', entity_id="fan.xiaomi_humidifier")
+        self.call_service('fan/turn_off', entity_id="fan.zhimi_humidifier_ca1")
         self.log('Humidifier switched off')
