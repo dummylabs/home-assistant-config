fbb3e93830afff0c0e8862ba6bf5bdb5476aa783
dummylabs <scratcha@gmail.com>
2020-01-12 19:30:02 +0000

    fixed a bug in humidifier, updated coffee app to esphome sensor

--- appdaemon/apps/coffee/coffee.py
+++ appdaemon/apps/coffee/coffee.py
@@ -1,27 +1,22 @@
 import appdaemon.plugins.hass.hassapi as hass
 import errors
 
+treshold = 40
+
 class CoffeeMaker(hass.Hass):
     def initialize(self):
         self.listen_state(self.state_handler, 'binary_sensor.coffee_maker')
         self.tts = self.get_app('tts')
         self.start = None
-        self.listen_event(self.coffee_ready_event, "esphome.coffee_ready")
-
-    def coffee_ready_event(self, entity, data, kwargs):
-        self.log(f'COFFEE is READY')
 
     def state_handler(self, entity, attribute, old, new, kwargs):
-        if old == 'off' and new == 'on':
+        if old < treshold and new >= treshold:
             self.start = now = self.datetime()
-        if old == 'on' and new == 'off' and self.start:
+        if old >= treshold and new < treshold and self.start:
             duration = (self.datetime()-self.start).total_seconds()
             self.log('Coffe Maker has been working for {} seconds'.format(duration))
-            if duration > 10 and duration < 100:
-                self.tts.speak('Возможно вы забыли добавить воду в кофеварку', volume=0.3, opener='notify1.mp3')
-            else:
-                self.tts.speak('Кофе готов.', volume=0.3, opener='notify1.mp3')
-
-
-            
-            
+            if duration > 10:
+                if duration < 100:
+                    self.tts.speak('Возможно вы забыли добавить воду в кофеварку', volume=0.3, opener='notify1.mp3')
+                else:
+                    self.tts.speak('Кофе готов.', volume=0.3, opener='notify1.mp3')
