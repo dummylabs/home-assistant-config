b573be75f21aeb20b561181bb5688849f31a1ffc
dummylabs <scratcha@gmail.com>
2020-02-29 14:58:22 +0300

    bugfixing

--- appdaemon/apps/switcher/shelf.py
+++ appdaemon/apps/switcher/shelf.py
@@ -13,9 +13,11 @@ class Shelf(hass.Hass):
     def get_value(self, what, default=None):
         mutex = Lock()
         mutex.acquire()
+        value = default
         try:
             with shelve.open(self.filename, 'c') as shelf:
-                value = shelf[what] if what in shelf else default
+                if what in shelf:
+                    value = shelf[what]
         except Exception as e:
             self.log(f'Exception occurred "{e}"', level = "ERROR")
         mutex.release()
