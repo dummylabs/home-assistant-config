06d534a94ec8dad81024bd0681c9d8ec2a9e9977
dummylabs <scratcha@gmail.com>
2020-03-01 15:33:56 +0300

    bedlight automations

---
+++ appdaemon/apps/light_routines/masha_bedlight.py
@@ -0,0 +1,15 @@
+import appdaemon.plugins.hass.hassapi as hass
+import datetime
+
+class MashaBedlight(hass.Hass):
+    def initialize(self):
+        self.entity_id = 'light.bedlight_masha' 
+        self.run_daily(self.switch_on, "21:50:00")
+        self.run_daily(self.switch_off, "sunrise")
+
+    def switch_on(self, kwargs):
+        self.call_service("light/turn_on", entity_id=self.entity_id)
+    def switch_off(self, kwargs):
+        self.call_service("light/turn_off", entity_id=self.entity_id)
+
+
